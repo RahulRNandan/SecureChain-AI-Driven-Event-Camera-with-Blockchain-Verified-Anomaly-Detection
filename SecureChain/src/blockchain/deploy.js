694c328952e7ec418b7fb8b7b0f1b70d6130ed2d
RahulRNandan <75572502+RahulRNandan@users.noreply.github.com>
2025-05-14 10:57:17 +0530

    Update deploy.js

--- SecureChain/src/blockchain/deploy.js
+++ SecureChain/src/blockchain/deploy.js
@@ -1,8 +1,26 @@
-// deploy.js - Deployment Script (Stub)
+const Web3 = require('web3');
+const fs = require('fs');
+const path = require('path');
+const AccessControlABI = JSON.parse(fs.readFileSync(path.resolve(__dirname, './contracts/AccessControl.json'))).abi;
+const EventLoggerABI = JSON.parse(fs.readFileSync(path.resolve(__dirname, './contracts/EventLogger.json'))).abi;
+
+const web3 = new Web3('http://localhost:8545');  // Example for local node
 
 async function deploy() {
-    console.log("Deploying contracts... [stub]");
-    // TODO: Add actual deployment logic
+    const accounts = await web3.eth.getAccounts();
+    const admin = accounts[0];
+
+    // Deploy AccessControl contract
+    const accessControl = await new web3.eth.Contract(AccessControlABI)
+        .deploy({ data: '0x' + fs.readFileSync(path.resolve(__dirname, './contracts/AccessControl.bin')).toString() })
+        .send({ from: admin, gas: 1500000 });
+    console.log('AccessControl contract deployed at:', accessControl.options.address);
+
+    // Deploy EventLogger contract
+    const eventLogger = await new web3.eth.Contract(EventLoggerABI)
+        .deploy({ data: '0x' + fs.readFileSync(path.resolve(__dirname, './contracts/EventLogger.bin')).toString() })
+        .send({ from: admin, gas: 1500000 });
+    console.log('EventLogger contract deployed at:', eventLogger.options.address);
 }
 
-deploy();
+deploy().catch(console.error);
