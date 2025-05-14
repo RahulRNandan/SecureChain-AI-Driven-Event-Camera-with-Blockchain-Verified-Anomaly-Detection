761f5085cb477822253ccd47dee49695ae5b2c7e
RahulRNandan <75572502+RahulRNandan@users.noreply.github.com>
2025-05-14 10:56:23 +0530

    Update AccessControl.sol

--- SecureChain/src/blockchain/AccessControl.sol
+++ SecureChain/src/blockchain/AccessControl.sol
@@ -1,1 +1,19 @@
-# TODO: Implement Accesscontrol functionality
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract AccessControl {
+    address public admin;
+    
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Access denied. Only admin allowed.");
+        _;
+    }
+
+    constructor() {
+        admin = msg.sender;
+    }
+
+    function setAdmin(address newAdmin) public onlyAdmin {
+        admin = newAdmin;
+    }
+}
